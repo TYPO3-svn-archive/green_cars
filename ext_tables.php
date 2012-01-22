2360c10400867de7eaa6c33fad83ee76b175b65b
dwildt <dwildt@735d13b6-9817-0410-8766-e36946ffe9aa>
2012-01-22 16:29:05 +0000

    1.3.0 - pagetree icon
    git-svn-id: https://svn.typo3.org/TYPO3v4/Extensions/green_cars/trunk@56572 735d13b6-9817-0410-8766-e36946ffe9aa

--- ext_tables.php
+++ ext_tables.php
@@ -2,6 +2,20 @@
 if (!defined ('TYPO3_MODE')) {
 	die ('Access denied.');
 }
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // INDEX
+
+  // TCA tables
+  // Enables the Include Static Templates
+  // Add pagetree icons
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // TCA tables
+
 $TCA['tx_greencars_manufacturer'] = array (
 	'ctrl' => array (
 		'title'     => 'LLL:EXT:green_cars/locallang_db.xml:tx_greencars_manufacturer',		
@@ -86,6 +100,26 @@ $TCA['tx_greencars_main'] = array (
 		'iconfile'          => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif',
 	),
 );
+  // TCA tables
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Enables the Include Static Templates
 
 t3lib_extMgm::addStaticFile($_EXTKEY,'static/browser/', 'Browser');
+  // Enables the Include Static Templates
+
+
+
+  ////////////////////////////////////////////////////////////////////////////
+  //
+  // Add pagetree icons
+
+$TCA['pages']['columns']['module']['config']['items'][] =
+  array('Green Cars', 'green_cars', t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif');
+$ICON_TYPES['green_cars'] = array('icon' => t3lib_extMgm::extRelPath($_EXTKEY).'ext_icon.gif');
+  // Add pagetree icons
+
 ?>
